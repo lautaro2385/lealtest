11eb219baf0ecd3f913dc01038045f541fc3b34d
Gustavo Alejandro <lautaro14@hotmail.com>
2019-03-08 00:36:46 -0500

    wip: some test

---
+++ transaction/test/fixtures/transaction.js
@@ -0,0 +1,12 @@
+'use strict'
+let transaction = {
+  value: 10.1,
+  points: 10,
+  status: 1,
+  user_id: "1"
+}
+
+
+module.exports = {
+  single: transaction
+}
---
+++ transaction/test/transaction-test.spec.js
@@ -0,0 +1,44 @@
+'use strict'
+const moment = require('moment');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const trFixtures = require('./fixtures/transaction');
+let db = null;
+
+chai.use(chaiAsPromised);
+const assert = chai.assert;
+const expect = chai.expect;
+
+let single = Object.assign({}, trFixtures.single)
+
+describe('pruebas de transacciones', () => {
+  describe('pruebas de trasnascciones DB', () => {
+    const db = require('../db/model')
+
+    beforeEach(async () => {
+      await db.sequalize.sync({ force: true })
+    })
+
+    it('la entidad de transaccion debe existir', () => {
+      assert(db.transaction)
+    })
+
+    it('crea transaccion en db', async () => {
+      let resp = await db.transaction.create(single)
+      assert.equal(resp.points, single.points)
+      assert.equal(resp.status, single.status)
+      assert.equal(resp.user_id, single.user_id)
+      assert.equal(resp.value, single.value)
+    })
+
+    it('trasacción#findById', async () => {
+      const resp = await db.transaction.create(single)
+      assert(resp, 'debe existir')
+      const tr = await db.transaction.findById(resp.transaction_id);
+      assert.equal(tr.points, single.points)
+      assert.equal(tr.status, single.status)
+      assert.equal(tr.user_id, single.user_id)
+      assert.equal(tr.value, single.value)
+    })
+  })
+})
